ebd3806c7e1ab688ed1f311b0cd50b4b5b04c913
tkchafin <tkc_815@live.com>
2016-03-10 19:01:24 -0600

    Modified build.bash

--- build.bash
+++ build.bash
@@ -1,3 +1,5 @@
 #!/bin/bash
 set -e -v
 javac src/Game.java src/Model.java src/View.java src/Controller.java src/Sprite.java src/Bird.java src/Tree.java src/Egg.java src/AmmoBar.java src/Plant.java
+
+mv src/*.class .
